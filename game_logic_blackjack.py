6c8f0739ea906270b2bb2affd23091c720b42db9
VasseDev <pietrovassena01234@gmail.com>
2022-12-18 20:37:45 +0100

    modified  game_logic_blackjack.py modified  main.py

--- game_logic_blackjack.py
+++ game_logic_blackjack.py
@@ -9,9 +9,7 @@ class Hand:
 
     def __init__(self, deck):
         self.first_hand = [deck.pop() for _ in range(2)]
-
-    def __str__(self):
-        return str(self.first_hand), str(self.second_hand) if self.second_hand else str(self.first_hand)
+        self.second_hand = []
 
     def __getitem__(self, index):
         return self.first_hand[index]
@@ -30,6 +28,9 @@ class Hand:
                 if element[1] == "Ace":
                     total -= 10
         return total
+
+    def get_cards(self):
+        return str(self.first_hand), str(self.second_hand)
 
     def draw_card(self, deck: list[tuple[str]]) -> None:
         self.first_hand.append(deck.pop())
@@ -54,7 +55,7 @@ class Player:
     hand: Hand
     fiches: int
 
-    def __init__(self, deck):
+    def __init__(self, deck: list[tuple[str]]):
         self.hand = Hand(deck)
         self.fiches = 100
 
@@ -64,103 +65,45 @@ class Player:
     def __int__(self):
         return self.fiches
 
+    def bet(self, amount: int) -> None:
+        self.fiches -= amount
+
+    def win(self, amount: int) -> None:
+        self.fiches += amount * 2
 
 
-''' controlla chi ha vinto tra il giocatore e il banco
-def check_winner(dealer_hand, player_hand):
-    if hand_sum(dealer_hand) > hand_sum(player_hand) and check_hand_sum(dealer_hand) != "Bust":
-        print("Dealer wins")
-    elif hand_sum(dealer_hand) < hand_sum(player_hand) and check_hand_sum(player_hand) != "Bust":
-        print("Player wins")
-    else:
-        print("Draw")
-'''
+class Dealer:
+
+    hand: Hand
+
+    def __init__(self, deck: list[tuple[str]]):
+        self.hand = Hand(deck)
+
+    def __str__(self):
+        return f'Hand: {str(self.hand)}'
+
+    def __int__(self):
+        return int(self.hand)
+
+    def draw_card(self, deck: list[tuple[str]]) -> None:
+        self.hand.draw_card(deck)
+
+    def check_hand_sum(self) -> str:
+        return self.hand.check_hand_sum()
 
 
-''' controlla la somma delle carte in mano del dealer
-def dealer_draws(deck, dealer_hand, first_hand, second_hand):
-    # controlla se il delaer deve pescare una carta per vincere
-    if (
-            (check_hand_sum(first_hand) not in ["Blackjack", "Bust"] or
-             check_hand_sum(second_hand) not in ["Blackjack", "Bust"]) and
-            check_hand_sum(dealer_hand) not in ["Blackjack", "Bust"] and
-            (hand_sum(dealer_hand) < hand_sum(first_hand) or
-             hand_sum(dealer_hand) < hand_sum(second_hand))
-    ):
-        print("Dealer draws a card")
-        dealer_hand.append(deck.pop())
-        print("Dealer hand: ", *dealer_hand)
-        dealer_draws(deck, dealer_hand, first_hand, second_hand)
-'''
+SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
+RANKS = ["2", "3", "4", "5", "6", "7", "8",
+         "9", "10", "Jack", "Queen", "King", "Ace"]
 
 
-'''chiede all'utente se vuole pescare o no
-def player_draws(deck, hand, splitted):
-    while check_hand_sum(hand) not in ["Blackjack", "Bust"]:
-        if (hand[0][1] == hand[1][1] or (hand[0][1] in RANKS[8:13] and hand[1][1] in RANKS[8:13])) and not (splitted):
-            choice = input("Hit/Stand/Double/Split: ").lower()
-        else:
-            choice = input("Hit/Stand/Double: ").lower()
-        if choice in ["hit", "double"]:
-            hand.append(deck.pop())
-            print("Player hand: ", *hand)
-        elif choice == "stand":
-            print("Sum: ", hand_sum(hand))
-            return
-        elif choice == "split":
-            return split_hand(hand, deck, player_draws)
-'''
+def create_deck() -> list[tuple[str]]:
+    deck = list(itertools.product(SUITS, RANKS))
+    deck += deck
+    random.shuffle(deck)
+    return deck
 
 
-# crea un mazzo e lo riempe con 104 carte
-
-def main() -> None:
-
-    SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    RANKS = ["2", "3", "4", "5", "6", "7", "8",
-             "9", "10", "Jack", "Queen", "King", "Ace"]
-
-    def create_deck() -> list[tuple[str]]:
-        deck = list(itertools.product(SUITS, RANKS))
-        deck += deck
-        random.shuffle(deck)
-        return deck
-
-    splitted = False
-    deck = create_deck()
-    player_hand = Hand(deck)
-    dealer_hand = Hand(deck)
-    print(f'Player hand: {player_hand}')
-    print(f'Dealer hand: [{dealer_hand[0]}, ("X", "X")]')
-
-    if dealer_hand.check_hand_sum() == 'Blackjack':
-        print(f'Dealer hand: {dealer_hand}')
-        print("Dealer wins")
-        return
-    if player_hand.check_hand_sum() == 'Blackjack':
-        print(f'Dealer hand: {dealer_hand}')
-        print("Player wins")
-        return
-
-    '''
-    if check_hand_sum(dealer_hand) != "Blackjack":
-        splitted = player_draws(deck, player_hand, splitted)
-        print("Dealer hand: ", *dealer_hand)
-        player_second_hand = []
-        dealer_draws(deck, dealer_hand, player_hand, player_second_hand)
-        # controlla chi ha vinto
-        if splitted:
-            print("First hand: ", end="")
-            check_winner(dealer_hand, player_hand)
-            print("Second hand: ", end="")
-            check_winner(dealer_hand, player_second_hand)
-        else:
-            check_winner(dealer_hand, player_hand)
-    else:
-        print("Dealer hand: ", *dealer_hand)
-        print("Dealer wins")
-    '''
-
-
-if __name__ == "__main__":
-    main()
+deck = create_deck()
+player = Player(deck)
+dealer_hand = Hand(deck)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter
-import game_logic_blackjack as glb
+import game_logic_blackjack as glbj  # game logic blowjob
+
 
 class App(customtkinter.CTk):
 
@@ -10,4 +11,29 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("Black Jack")
-        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")        
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+
+        self.player = glbj.Player(glbj.deck)
+        self.dealer = glbj.Dealer(glbj.deck)
+
+        # configure grid layout (11x9)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+        self.grid_rowconfigure(6, weight=1)
+        self.grid_rowconfigure(8, weight=1)
+        self.grid_rowconfigure(10, weight=1)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(4, weight=1)
+        self.grid_columnconfigure(6, weight=1)
+        self.grid_columnconfigure(8, weight=1)
+
+        # configure frames
+        
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
